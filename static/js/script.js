513bd95050b9cb6bea43faa1be6ef55400d5efb0
Shoaib Khan <shoaiba.khan@mail.utoronto.ca>
2023-10-19 17:39:31 -0400

    Front end MVP design complete

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// sendButton.addEventListener('click', function() {
+//     var message = messageInput.value;
+//     if (message !== '') {
+//         socket.emit('new_message', {message: message});
+//     }
+// });
